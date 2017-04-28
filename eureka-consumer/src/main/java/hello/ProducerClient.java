a6e7a6317f380791dc62a84d56d5161330dd4724
Dariusz Zięba <Dariusz.Zieba@zooplus.com>
2017-04-28 13:16:27 +0200

    Feign support hystrix

--- eureka-consumer/src/main/java/hello/ProducerClient.java
+++ eureka-consumer/src/main/java/hello/ProducerClient.java
@@ -4,9 +4,17 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("eureka-producer")
+@FeignClient(name = "eureka-producer", fallback = HystrixClientFallback.class)
 public interface ProducerClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/info")
     String info();
+}
+
+class HystrixClientFallback implements ProducerClient {
+
+    @Override
+    public String info() {
+        return "Fallback";
+    }
 }
